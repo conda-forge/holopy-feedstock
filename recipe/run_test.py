eec9df532a2b2da5f3b8a16b7ccb0e92e1fb6160
ralex0 <ralex0@users.noreply.github.com>
2019-11-10 23:12:31 -0500

    Merge in windows build fixing changes

--- recipe/run_test.py
+++
@@ -1,7 +0,0 @@
-#Confirm import
-import holopy
-#Confirm main Fortran code compiled properly
-from holopy.scattering.theory.mie_f import mieangfuncs, scsmfo_min
-#Confirm Tmatrix Fortran code compiled properly
-from holopy.scattering import Tmatrix
-Tmatrix()
